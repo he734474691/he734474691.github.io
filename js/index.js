b63ccb7ccdc102e67a4eab4c30849662d20825fa
he734474691 <734474691@qq.com>
2019-10-17 09:13:07 +0800

    33

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-var oP = document.createElement('p');
-    var data = document.getElementsByClassName('data');
-    var aa = document.data.appendChild(oP);
-    setInterval(function(){
-        var myDate = new Date();
-        var y = myDate.getFullYear();
-        var m = myDate.getMonth()+1;
-        var d = myDate.getDate();
-        var h = myDate.getHours();
-        var minu = myDate.getMinutes();
-        var s = myDate.getSeconds();
-        oP.innerHTML = '今天是'+ y +'年' + m + '月' + d + '日' + h +':'+towNum(minu)+ ':' +towNum(s);
-    },1000);
-    function towNum(n){
-        return n > 9 ? n:'0'+n;
-    }
+function towNum(n) {
+    return n > 9 ? n : '0' + n;
+}
+var oSpan = document.createElement('span');
+var date = document.getElementById('date');
+var aa = date.appendChild(oSpan);
+setInterval(function () {
+    var myDate = new Date();
+    var y = myDate.getFullYear();
+    var m = myDate.getMonth() + 1;
+    var d = myDate.getDate();
+    var h = myDate.getHours();
+    var minu = myDate.getMinutes();
+    var s = myDate.getSeconds();
+    oSpan.innerHTML =  y + '/' + m + '/' + d + '/' + h + ':' + towNum(minu) + ':' + towNum(s);
+}, 250);
